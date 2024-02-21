7c91c803698ee9a700f99235c4a2d0b1c16cacd8
eCost <ammymalik2221@gmail.com>
2024-02-21 10:28:26 +0530

    First Day

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,18 +5,20 @@ plugins {
 
 android {
     namespace = "ininc.foodmarket"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ininc.foodmarket"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    buildFeatures {
+        viewBinding = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
